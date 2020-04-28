e33b3292cacbbf1ba5084681614864a6fea67354
Érick do Nascimento Garcia <erickhssp@outlook.com>
2020-04-28 11:36:56 -0300

    Validando CPF

--- ValidadorDeDocumentos/Program.cs
+++ ValidadorDeDocumentos/Program.cs
@@ -12,6 +12,26 @@ namespace ValidadorDeDocumentos
     {
         static void Main(string[] args)
         {
+            string cpf1 = "86288366757";
+            string cpf2 = "98745366797";
+            string cpf3 = "22222222222";
+
+            ValidarCPF(cpf1);
+            ValidarCPF(cpf2);
+            ValidarCPF(cpf3);
+        }
+
+        private static void ValidarCPF(string cpf)
+        {
+            try
+            {
+                new CPFValidator().AssertValid(cpf);
+                Debug.WriteLine("CPF válido: " + cpf);
+            }
+            catch (Exception exc)
+            {
+                Debug.WriteLine("CPF inválido: " + cpf + " : " + exc.ToString());
+            }
         }
     }
 }
