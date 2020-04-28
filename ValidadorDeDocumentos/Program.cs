ccec26cc2c206b67cf601b5e1463edd9d7a67101
Érick do Nascimento Garcia <erickhssp@outlook.com>
2020-04-28 11:54:57 -0300

    Formatando documentos

--- ValidadorDeDocumentos/Program.cs
+++ ValidadorDeDocumentos/Program.cs
@@ -1,10 +1,7 @@
-﻿using Caelum.Stella.CSharp.Validation;
+﻿using Caelum.Stella.CSharp.Format;
+using Caelum.Stella.CSharp.Validation;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ValidadorDeDocumentos
 {
@@ -12,6 +9,7 @@ namespace ValidadorDeDocumentos
     {
         static void Main(string[] args)
         {
+
             string cpf1 = "86288366757";
             string cpf2 = "98745366797";
             string cpf3 = "22222222222";
@@ -20,18 +18,29 @@ namespace ValidadorDeDocumentos
             ValidarCPF(cpf2);
             ValidarCPF(cpf3);
 
-
             string cnpj1 = "51241758000152";
-            string cnpj2 = "14128481000120";
+            string cnpj2 = "14128481000127";
 
             ValidarCNPJ(cnpj1);
             ValidarCNPJ(cnpj2);
 
             string titulo1 = "041372570132";
-            string titulo2 = "774387480132";
+            string titulo2 = "774387480130";
 
             ValidarTitulo(titulo1);
             ValidarTitulo(titulo2);
+
+            Debug.WriteLine(cpf1);
+            string cpfFormatado = new CPFFormatter().Format(cpf1);
+            Debug.WriteLine(cpfFormatado);
+            Debug.WriteLine(new CPFFormatter().Format(cpfFormatado));
+            Debug.WriteLine(new CPFFormatter().Unformat(cpfFormatado));
+
+            Debug.WriteLine(cnpj1);
+            Debug.WriteLine(new CNPJFormatter().Format(cnpj1));
+
+            Debug.WriteLine(titulo1);
+            Debug.WriteLine(new TituloEleitoralFormatter().Format(titulo1));
         }
 
         private static void ValidarTitulo(string titulo)
