ac4eb7105383c1b128f4714b8d55a71f04c97d1d
Érick do Nascimento Garcia <erickhssp@outlook.com>
2020-04-28 12:31:04 -0300

    Trabalhando com Datas e Horas

---
+++ TrabalhandoComDatas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhandoComDatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime data = new DateTime(2020, 04, 28);
+            Debug.WriteLine(data);
+            Debug.WriteLine(data.ToString("d"));
+            Debug.WriteLine(data.ToString("d", new CultureInfo("pt-BR")));
+            Debug.WriteLine(data.ToString("dd/MM"));
+            Debug.WriteLine(data.ToString("dd/MM/yy"));
+
+            data = new DateTime(2020, 04, 28, 13, 14, 31,987);
+            Debug.WriteLine(data);
+            Debug.WriteLine(data.ToString("HH:mm"));
+            Debug.WriteLine(data.ToString("HH:mm:ss.fff"));
+
+
+        }
+    }
+}
