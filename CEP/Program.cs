2e069a96f52b32651d530e7cd61b63e416252758
Érick do Nascimento Garcia <erickhssp@outlook.com>
2020-04-28 18:07:17 -0300

    Consultando o Webservice do ViaCEP

---
+++ CEP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cep = "010010007";
+            string url = "http://viacep.com.br/ws/" + cep + "/json/";
+
+            var result = new HttpClient().GetStringAsync(url).Result;
+
+            Debug.WriteLine(result);
+
+        }
+    }
+}
