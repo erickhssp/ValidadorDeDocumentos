e2341d18f1d3c6b3ad97623093443e6b94ab2953
Érick do Nascimento Garcia <erickhssp@outlook.com>
2020-04-28 18:56:44 -0300

    Consultando ViaCEP Com Caelum Stella CSharp

--- CEP/Program.cs
+++ CEP/Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Caelum.Stella.CSharp.Http;
 using System.Diagnostics;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CEP
 {
@@ -12,13 +8,30 @@ namespace CEP
     {
         static void Main(string[] args)
         {
-            string cep = "010010007";
-            string url = "http://viacep.com.br/ws/" + cep + "/json/";
-
-            var result = new HttpClient().GetStringAsync(url).Result;
-
+            string cep = "01001000";
+            string result = GetEndereco(cep);
             Debug.WriteLine(result);
 
+            ViaCEP viaCEP = new ViaCEP();
+            string enderecoJson = viaCEP.GetEnderecoJson(cep);
+            Debug.WriteLine(enderecoJson);
+
+            string enderecoXml = viaCEP.GetEnderecoXml(cep);
+            Debug.WriteLine(enderecoXml);
+
+            var task = viaCEP.GetEnderecoJsonAsync(cep);
+            Debug.WriteLine(task.Result);
+
+            var endereco = viaCEP.GetEndereco(cep);
+            Debug.WriteLine(string.Format("Logradouro: {0}, Bairro: {1}", endereco.Logradouro, endereco.Bairro));
+        }
+
+        private static string GetEndereco(string cep)
+        {
+            string url = "https://viacep.com.br/ws/" + cep + "/json/";
+
+            string result = new HttpClient().GetStringAsync(url).Result;
+            return result;
         }
     }
 }
